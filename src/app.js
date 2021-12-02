991c3c3098c49d765bb356a960b2051d14ada4bb
Damir Khalimov <xalimovdamir@gmail.com>
2021-12-02 11:57:44 +0500

    heroku preparations

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -76,6 +77,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('App running on 3000 port');
+app.listen(port, () => {
+  console.log('App running on port ' + port);
 })
